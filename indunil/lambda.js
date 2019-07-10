e20088def574ce2f6d718195042f81b20f1744f4
Indunil1992 <indunil@adroitlogic.com>
2019-07-10 18:21:18 +0530

    jil

--- indunil/lambda.js
+++ indunil/lambda.js
@@ -4,33 +4,34 @@ const sqs = new SL_AWS.SQS(AWS);
 
 exports.handler = function (event, context, callback) {
 
-    sqs.sendMessage({
-        MessageBody: 'test put',
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Hiru1T`,
-        DelaySeconds: '0',
-        MessageAttributes: {
-            "1": {
-                "DataType": "String",
-                "StringValue": "abc"
-            },
-            "2": {
-                "DataType": "Number",
-                "StringValue": "123"
-            },
-            "3": {
-                "DataType": "Binary",
-                "BinaryValue": "0"
+    sqs.receiveMessage({
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/run`,
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '10',
+        VisibilityTimeout: '15',
+        WaitTimeSeconds: '0',
+        MessageAttributeNames: ['a', 's', 'd']
+    }).promise()
+        .then(receivedMsgData => {
+            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
+                let receivedMessages = receivedMsgData.Messages;
+                receivedMessages.forEach(message => {
+                    console.log("Success message");
+                 console.log( message );
+                    // your logic to access each message through out the loop. Each message is available under variable message 
+                    // within this block
+                });
+            } else {
+                console.log("no msgesss");
+                // console.log( data );
+                // No messages to process
             }
-        }
-    }, function (data) {
-        console.log("Success data");
-                 console.log( data );
-        // your logic (logging etc) to handle successful message delivery, should be here
-    }, function (error) {
-        console.log("not Success");
-                 console.log( error );
-        // your logic (logging etc) to handle failures, should be here
-    });
+        })
+        .catch(err => {
+            console.log("not Success");
+                 console.log( err );
+            // error handling goes here
+        });
 
 
 
