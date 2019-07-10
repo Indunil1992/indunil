e120b9df75494610fca92b79849eeb4174e8b01d
Indunil1992 <indunil@adroitlogic.com>
2019-07-10 12:58:30 +0530

    ui

--- indunil/lambda.js
+++ indunil/lambda.js
@@ -1,5 +1,40 @@
-exports.handler = function(event, context, callback) {
-    
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+let SL_AWS = require('slappforge-sdk-aws');
+const sqs = new SL_AWS.SQS(AWS);
+
+exports.handler = function (event, context, callback) {
+
+    sqs.sendMessage({
+        MessageBody: 'test put',
+        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Hiru1T`,
+        DelaySeconds: '0',
+        MessageAttributes: {
+            "1": {
+                "DataType": "String",
+                "StringValue": "abc"
+            },
+            "2": {
+                "DataType": "Number",
+                "StringValue": "123"
+            },
+            "3": {
+                "DataType": "Binary",
+                "BinaryValue": "0"
+            }
+        }
+    }, function (data) {
+        console.log("Success data");
+                 console.log( data );
+        // your logic (logging etc) to handle successful message delivery, should be here
+    }, function (error) {
+        console.log("not Success");
+                 console.log( error );
+        // your logic (logging etc) to handle failures, should be here
+    });
+
+
+
+
+
+    callback(null, { "message": "Successfully executed" });
 }
