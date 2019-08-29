9ebbf27f8de197d7d70fdfaa4c78086bf87b6675
Indunil1992 <indunil@adroitlogic.com>
2019-08-29 17:27:02 +0530

    a

--- indunil/lambda.js
+++ indunil/lambda.js
@@ -1,37 +1,4 @@
-let AWS = require('aws-sdk');
-let SL_AWS = require('slappforge-sdk-aws');
-const sqs = new SL_AWS.SQS(AWS);
-
-exports.handler = function (event, context, callback) {
-    sqs.receiveMessage({
-        QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/Hiru1T`,
-        AttributeNames: ['All'],
-        MaxNumberOfMessages: '1',
-        VisibilityTimeout: '30',
-        WaitTimeSeconds: '10',
-        MessageAttributeNames: ['a', 's', 'd']
-    }).promise()
-        .then(receivedMsgData => {
-            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
-                let receivedMessages = receivedMsgData.Messages;
-                receivedMessages.forEach(message => {
-                    console.log("msg Success");
-                 console.log( message );
-                    // your logic to access each message through out the loop. Each message is available under variable message 
-                    // within this block
-                });
-            } else {
-                console.log("nooo Success");
-                 //console.log( data );
-                // No messages to process
-            }
-        })
-        .catch(err => {
-            console.log("err Success");
-                 console.log( err );
-            // error handling goes here
-        });
-
-
-    callback(null, { "message": "Successfully executed" });
+exports.handler = function(event, context, callback) {
+    console.log("test API")
+    callback(null, {"message": "Successfully executed"});
 }
